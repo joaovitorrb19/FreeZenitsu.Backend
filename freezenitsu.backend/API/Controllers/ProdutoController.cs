e09b08400153eb0605a3a14a28a3a19fed837280
Joao Vitor <joaovitor.rb19@gmail.com>
2023-07-09 20:27:15 -0300

    ainda não feito a parte do produto

---
+++ freezenitsu.backend/API/Controllers/ProdutoController.cs
@@ -0,0 +1,57 @@
+﻿using App.InputModel;
+using Core.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("")]
+    public class ProdutoController : Controller
+    {
+        public ProdutoController()
+        {
+                
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("getbyid")]
+        public IActionResult GetById([FromBody]int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("getbyname")]
+        public IActionResult GetByName([FromBody]string name)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("post")]
+        public IActionResult Post([FromBody]ProdutoInputModel produto)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("put")]
+        public IActionResult Put([FromBody] ProdutoInputModel produto)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public IActionResult Delete([FromBody] ProdutoInputModel produto)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ freezenitsu.backend/App/InputModel/ProdutoInputModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.InputModel
+{
+    public class ProdutoInputModel
+    {
+        public string Nome { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public IFormFile Imagem { get; set; }
+    }
+}
